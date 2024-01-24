1f1a62817201d68d2205901920ef89827097d964
chirag kanani <107808217+kananichirag@users.noreply.github.com>
2024-01-24 21:20:16 +0530

    connection to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.listen(3000, () => console.log("Its Work...!!!"));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((e) => console.log("Mongodb Connected.!!"))
+  .catch((err) => console.log("Connecting Error ==>", err));
+
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`Server Start at ${process.env.PORT}`)
+);
