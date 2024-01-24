64dcb05b260e2fbc584b575635c72ad7d97c23ef
chirag kanani <107808217+kananichirag@users.noreply.github.com>
2024-01-24 20:47:04 +0530

    create and server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -33,7 +33,7 @@ function Header() {
           <IoIosMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
         </Link>
         <NavbarToggle />
       </div>
